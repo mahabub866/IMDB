fec61b146c9d394904e09a5bbcb74543863ce8e4
Mahabobur Rahman <mahaboburrahman866@gmail.com>
2022-07-18 13:14:58 +0600

    env setup

--- imdb_clone/imdb_env/Scripts/activate.bat
+++ imdb_clone/imdb_env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=F:\Project\Python\IMDB\imdb_clone\imdb_env
+set VIRTUAL_ENV=D:\Python Django\IMDB\imdb_clone\imdb_env
 
 if not defined PROMPT set PROMPT=$P$G
 
